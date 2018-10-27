86650f3467ba0775bd5af7396904168984ec55fd
Raj Tandukar <royal_raj@outlook.com>
2018-10-27 14:08:33 +0545

    adds details to home view

--- LMS/Controllers/HomeController.cs
+++ LMS/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using LMS.Models;
 using LMS.ViewModel;
+using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,30 +13,78 @@ namespace LMS.Controllers
     public class HomeController : Controller
     {
         ApplicationDbContext db = new ApplicationDbContext();
+        BookLoanVM BookLoan = new BookLoanVM();
 
         public ActionResult Index(string option, string search)
         {
+            List<Book> books = new List<Book>();
             if (option == "Book")
             {
-                var books = db.Books.Where(x=>x.Name==search||search==null).ToList();
-                
-                return View(books);
+
+                books = db.Books.Where(x => x.Name == search || search == null).ToList();
+                //var booksOnShelf = from l in db.Loans join b in db.Books on l.BookId equals b.Id join a in db.Authors on b.AuthorId equals a.Id join p in db.Publishers on b.PublisherId equals p.Id where b.Name == search select new { b, l, a, p };
+
             }
             else if (option == "Author")
             {
-                var books = db.Books.Where(x => x.Authors.FirstName == search || search == null).ToList();
+                books = db.Books.Where(x => x.Authors.FirstName == search || search == null).ToList();
 
-                return View(books);
             }
             else
             {
-                var books = db.Books.Where(x => x.Publishers.Name == search || search == null).ToList();
+                books = db.Books.Where(x => x.Publishers.Name == search || search == null).ToList();
 
-                return View(books);
             }
-            
+            BookLoan.Books = books;
+            //BooksOnLoan(option,search);
+
+
+            return View(BookLoan);
+
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            BookLoan = new BookLoanVM();
+            ViewBag.OnShelf = true;
+            var booksOnLoan = (from l in db.Loans join b in db.Books on l.BookId equals b.Id where b.Id == id select new { l }).ToList().Select(x => x.l).ToList();
+            BookLoan.Loans = booksOnLoan;
+            BookLoan.Book = book;
+            return View(BookLoan);
         }
 
+        //public void BooksOnLoan()
+        //{
+        //    ViewBag.OnShelf = true;
+        //    var booksOnLoan = (from l in db.Loans join b in db.Books on l.BookId equals b.Id where b.Name == search select new { l }).ToList().Select(x => x.l).ToList();
+        //    BookLoan.Loans = booksOnLoan;
+            //else if (option == "Author")
+            //{
+            //    ViewBag.OnShelf = true;
+            //    var booksOnLoan = (from l in db.Loans join a in db.Authors on l.BookId equals a.Id where a.FirstName == search select new { l }).ToList().Select(x => x.l).ToList();
+            //    BookLoan.Loans = booksOnLoan;
+            //}
+            //else if (option == "Publisher")
+            //{
+            //    ViewBag.OnShelf = true;
+            //    var booksOnLoan = (from l in db.Loans join p in db.Publishers on l.BookId equals p.Id where p.Name == search select new { l }).ToList().Select(x => x.l).ToList();
+            //    BookLoan.Loans = booksOnLoan;
+            //}
+
+
+
+            //var totalBooksOnLoan = Convert.ToInt32(booksOnLoan);
+
+        //}
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ LMS/ViewModel/BookLoanVM.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.ViewModel
+{
+    public class BookLoanVM
+    {
+        public Loan Loan { get; set; }
+        public Book Book { get; set; }
+        public List<Loan> Loans { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
